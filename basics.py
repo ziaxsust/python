488c999414fa3e59a1b4dd831555de9ce0ab5b0d
Ziaul Karim <ziaulkarim@Ziauls-MacBook-Pro.local>
2025-11-18 20:03:35 -0500

    first python commit

---
+++ basics.py
@@ -0,0 +1,24 @@
+# how to use print function
+print(3+5)
+
+# Variables & Data types:
+name = "Ziaul Karim"                        #string
+age = 38                                    #integer
+weight = 88.5                               #float
+is_Married = True                           #boolean
+offspring = ['Sarah', 3, 'Musab', 1.5]      #list
+
+print(name, age, weight, is_Married)
+print(offspring)
+print(offspring[1])
+print(offspring[0])
+
+# Math Operations:
+a = 4
+b = 2
+print('Addition =', + a+b)
+print('Subtraction =', a-b)
+print('Multiplication =', a*b)
+print('Division =', a/b)
+print('Remainder =', a%b)
+print('Exponent =', a**b)
